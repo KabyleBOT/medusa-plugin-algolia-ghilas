bd15d24d27c0acadcd6e78f5251c4dd1b303f438
KabyleBOT <khaled.belkouche@gmail.com>
2024-02-09 16:33:34 +0100

    Bump version to 0.0.6 in package.json

--- src/subscribers/product.ts
+++ src/subscribers/product.ts
@@ -1,204 +1,204 @@
-import {
-	ProductService,
-	ProductVariantService,
-	Logger,
-	ProductStatus,
-} from "@medusajs/medusa";
-import {
-	IEventBusService,
-	ISearchService,
-} from "@medusajs/types";
-import { defaultSearchIndexingProductRelations } from "@medusajs/utils";
-import { indexTypes } from "medusa-core-utils";
+// import {
+// 	ProductService,
+// 	ProductVariantService,
+// 	Logger,
+// 	ProductStatus,
+// } from "@medusajs/medusa";
+// import {
+// 	IEventBusService,
+// 	ISearchService,
+// } from "@medusajs/types";
+// import { defaultSearchIndexingProductRelations } from "@medusajs/utils";
+// import { indexTypes } from "medusa-core-utils";
 
-const productRelations = [
-	...defaultSearchIndexingProductRelations,
-	"categories",
-];
+// const productRelations = [
+// 	...defaultSearchIndexingProductRelations,
+// 	"categories",
+// ];
 
-type InjectedDependencies = {
-	eventBusService: IEventBusService;
-	searchService: ISearchService;
-	productService: ProductService;
-	logger: Logger;
-};
+// type InjectedDependencies = {
+// 	eventBusService: IEventBusService;
+// 	searchService: ISearchService;
+// 	productService: ProductService;
+// 	logger: Logger;
+// };
 
-export default class ProductSearchSubscriber {
-	private readonly eventBusService_: IEventBusService;
-	private readonly searchService_: ISearchService;
-	private readonly productService_: ProductService;
-	private readonly logger_: Logger;
+// export default class ProductSearchSubscriber {
+// 	private readonly eventBusService_: IEventBusService;
+// 	private readonly searchService_: ISearchService;
+// 	private readonly productService_: ProductService;
+// 	private readonly logger_: Logger;
 
-	constructor(
-		container: InjectedDependencies
-	) {
-		this.eventBusService_ =
-			container.eventBusService;
-		this.searchService_ =
-			container.searchService;
-		this.productService_ =
-			container.productService;
-		this.logger_ = container.logger;
+// 	constructor(
+// 		container: InjectedDependencies
+// 	) {
+// 		this.eventBusService_ =
+// 			container.eventBusService;
+// 		this.searchService_ =
+// 			container.searchService;
+// 		this.productService_ =
+// 			container.productService;
+// 		this.logger_ = container.logger;
 
-		this.eventBusService_.subscribe(
-			ProductService.Events.UPDATED,
-			this.handleProductUpdate
-		);
-		this.eventBusService_.subscribe(
-			ProductService.Events.CREATED,
-			this.handleProductCreation
-		);
-		this.eventBusService_.subscribe(
-			ProductService.Events.DELETED,
-			this.handleProductDeletion
-		);
-		this.eventBusService_.subscribe(
-			ProductVariantService.Events
-				.CREATED,
-			this.handleProductVariantChange
-		);
-		this.eventBusService_.subscribe(
-			ProductVariantService.Events
-				.UPDATED,
-			this.handleProductVariantChange
-		);
-		this.eventBusService_.subscribe(
-			ProductVariantService.Events
-				.DELETED,
-			this.handleProductVariantChange
-		);
-	}
+// 		this.eventBusService_.subscribe(
+// 			ProductService.Events.UPDATED,
+// 			this.handleProductUpdate
+// 		);
+// 		this.eventBusService_.subscribe(
+// 			ProductService.Events.CREATED,
+// 			this.handleProductCreation
+// 		);
+// 		this.eventBusService_.subscribe(
+// 			ProductService.Events.DELETED,
+// 			this.handleProductDeletion
+// 		);
+// 		this.eventBusService_.subscribe(
+// 			ProductVariantService.Events
+// 				.CREATED,
+// 			this.handleProductVariantChange
+// 		);
+// 		this.eventBusService_.subscribe(
+// 			ProductVariantService.Events
+// 				.UPDATED,
+// 			this.handleProductVariantChange
+// 		);
+// 		this.eventBusService_.subscribe(
+// 			ProductVariantService.Events
+// 				.DELETED,
+// 			this.handleProductVariantChange
+// 		);
+// 	}
 
-	handleProductCreation = async (
-		data
-	) => {
-		this.logger_.info(
-			"Algolia util Handling product creation"
-		);
-		try {
-			const product =
-				await this.productService_.retrieve(
-					data.id,
-					{
-						relations: productRelations,
-					}
-				);
-			await this.searchService_.addDocuments(
-				ProductService.IndexName,
-				[product],
-				indexTypes.products
-			);
+// 	handleProductCreation = async (
+// 		data
+// 	) => {
+// 		this.logger_.info(
+// 			"Algolia util Handling product creation"
+// 		);
+// 		try {
+// 			const product =
+// 				await this.productService_.retrieve(
+// 					data.id,
+// 					{
+// 						relations: productRelations,
+// 					}
+// 				);
+// 			await this.searchService_.addDocuments(
+// 				ProductService.IndexName,
+// 				[product],
+// 				indexTypes.products
+// 			);
 
-			this.logger_.info(
-				"Added 1 product to search index"
-			);
-		} catch (error) {
-			this.logger_.error(
-				"Failed adding product to search index",
-				error
-			);
-		}
-	};
+// 			this.logger_.info(
+// 				"Added 1 product to search index"
+// 			);
+// 		} catch (error) {
+// 			this.logger_.error(
+// 				"Failed adding product to search index",
+// 				error
+// 			);
+// 		}
+// 	};
 
-	handleProductUpdate = async (
-		data
-	): Promise<any> => {
-		this.logger_.info(
-			"Algolia util Handling product update"
-		);
-		try {
-			const { id } = data;
-			const product =
-				await this.productService_.retrieve(
-					id,
-					{
-						relations: productRelations,
-					}
-				);
+// 	handleProductUpdate = async (
+// 		data
+// 	): Promise<any> => {
+// 		this.logger_.info(
+// 			"Algolia util Handling product update"
+// 		);
+// 		try {
+// 			const { id } = data;
+// 			const product =
+// 				await this.productService_.retrieve(
+// 					id,
+// 					{
+// 						relations: productRelations,
+// 					}
+// 				);
 
-			if (
-				product.status !==
-				ProductStatus.PUBLISHED
-			) {
-				this.logger_.info(
-					"Removing product from index because it was unpublished"
-				);
-				await this.searchService_.deleteDocument(
-					ProductService.IndexName,
-					id
-				);
-				return true;
-			} else {
-				await this.searchService_.addDocuments(
-					ProductService.IndexName,
-					[product],
-					indexTypes.products
-				);
-				this.logger_.info(
-					"Published 1 product to search index"
-				);
-			}
-		} catch (error) {
-			this.logger_.error(
-				"Failed to update product to search index",
-				error
-			);
-		}
+// 			if (
+// 				product.status !==
+// 				ProductStatus.PUBLISHED
+// 			) {
+// 				this.logger_.info(
+// 					"Removing product from index because it was unpublished"
+// 				);
+// 				await this.searchService_.deleteDocument(
+// 					ProductService.IndexName,
+// 					id
+// 				);
+// 				return true;
+// 			} else {
+// 				await this.searchService_.addDocuments(
+// 					ProductService.IndexName,
+// 					[product],
+// 					indexTypes.products
+// 				);
+// 				this.logger_.info(
+// 					"Published 1 product to search index"
+// 				);
+// 			}
+// 		} catch (error) {
+// 			this.logger_.error(
+// 				"Failed to update product to search index",
+// 				error
+// 			);
+// 		}
 
-		return true;
-	};
+// 		return true;
+// 	};
 
-	handleProductDeletion = async (
-		data
-	) => {
-		this.logger_.info(
-			"Algolia util Handling product deletion"
-		);
-		try {
-			await this.searchService_.deleteDocument(
-				ProductService.IndexName,
-				data.id
-			);
-		} catch (error) {
-			this.logger_.error(
-				"Failed to remove product from index",
-				error
-			);
-		}
+// 	handleProductDeletion = async (
+// 		data
+// 	) => {
+// 		this.logger_.info(
+// 			"Algolia util Handling product deletion"
+// 		);
+// 		try {
+// 			await this.searchService_.deleteDocument(
+// 				ProductService.IndexName,
+// 				data.id
+// 			);
+// 		} catch (error) {
+// 			this.logger_.error(
+// 				"Failed to remove product from index",
+// 				error
+// 			);
+// 		}
 
-		this.logger_.info(
-			"Removed product from search index"
-		);
-	};
+// 		this.logger_.info(
+// 			"Removed product from search index"
+// 		);
+// 	};
 
-	handleProductVariantChange = async (
-		data
-	) => {
-		this.logger_.info(
-			"Algolia util Handling product variant change"
-		);
-		try {
-			const product =
-				await this.productService_.retrieve(
-					data.product_id,
-					{
-						relations: productRelations,
-					}
-				);
-			await this.searchService_.addDocuments(
-				ProductService.IndexName,
-				[product],
-				indexTypes.products
-			);
+// 	handleProductVariantChange = async (
+// 		data
+// 	) => {
+// 		this.logger_.info(
+// 			"Algolia util Handling product variant change"
+// 		);
+// 		try {
+// 			const product =
+// 				await this.productService_.retrieve(
+// 					data.product_id,
+// 					{
+// 						relations: productRelations,
+// 					}
+// 				);
+// 			await this.searchService_.addDocuments(
+// 				ProductService.IndexName,
+// 				[product],
+// 				indexTypes.products
+// 			);
 
-			this.logger_.info(
-				"Updated product in search index for variant update"
-			);
-		} catch (error) {
-			this.logger_.error(
-				"Failed to update search index for variant update",
-				error
-			);
-		}
-	};
-}
+// 			this.logger_.info(
+// 				"Updated product in search index for variant update"
+// 			);
+// 		} catch (error) {
+// 			this.logger_.error(
+// 				"Failed to update search index for variant update",
+// 				error
+// 			);
+// 		}
+// 	};
+// }
