5b5171395a51e69dd9e705080a642114a74d0dd9
KabyleBOT <khaled.belkouche@gmail.com>
2024-03-25 02:23:19 +0100

    Update package version and add new product relations

--- src/utils/productRelations.ts
+++ src/utils/productRelations.ts
@@ -3,4 +3,8 @@ import { defaultSearchIndexingProductRelations } from "@medusajs/utils";
 export const productRelations = [
 	...defaultSearchIndexingProductRelations,
 	"categories",
+	"custom_attributes",
+	"custom_attributes",
+	"attribute_values",
+	"int_attribute_values",
 ];
